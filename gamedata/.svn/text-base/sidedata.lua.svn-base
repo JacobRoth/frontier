e6cf10f2c0e24b4c37d48a9361cb9063a02afc61
Yanom <yanom@rocketmail.com>
2012-09-08 11:33:14 -0500

    added all the stuff for Frontier, my fork of CT

---
+++ gamedata/.svn/text-base/sidedata.lua.svn-base
@@ -0,0 +1,13 @@
+-- Defines the playable factions.
+local
+ sideData = {
+  [1] = {
+    name      = 'NKG',
+    startunit = 'bprimarycruiser',
+  },
+  [2] = {
+    name      = 'Drone',
+   startunit = 'kdroneengineer',
+  },
+}
+return sideData
---
+++ modinfo.lua
@@ -0,0 +1,17 @@
+local modinfo = {
+        name = "Frontier",
+        shortname = "fn",
+        version = "$VERSION",
+        game = "Conflict Terra",
+        shortgame = "fn",
+        description = "Frontier",
+        url = "",
+        
+        modtype = "1",
+
+        depend = {
+                "cursors.sdz",
+        }
+}
+ 
+return modinfo 
