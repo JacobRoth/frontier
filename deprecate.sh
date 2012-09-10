a09d279fd2281c0d7779eddea8944be9d409cfb7
yanom <yanom@usb-hdd-ubuntu.(none)>
2012-09-10 10:08:36 -0500

    added deprecate.sh to deprecate a given unit

---
+++ deprecate.sh
@@ -0,0 +1,5 @@
+git mv units/$1.lua unusedUnits/
+#git mv scripts/$1.lua unusedScripts/
+#git mv objects3d/$1.s3o unusedObjects3d/
+#git mv unitpics/$1.png unusedUnitpics/
+#git mv unittextures/"$1"*.tga unusedUnittextures/
