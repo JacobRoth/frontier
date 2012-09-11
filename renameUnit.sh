b22dffd5644b741eab5ebd6e19e4e540867337df
yanom <yanom@usb-hdd-ubuntu.(none)>
2012-09-11 10:09:53 -0500

    trying to get renameunit working

--- renameUnit.sh
+++ renameUnit.sh
@@ -1,2 +1,17 @@
+#!/bin/bash
+
+PATTERN=$1
+REPLACEMENT=$2
+
+IFS=$'\t'
+
+while read DIR FILE; do
+        if ["$DIR/$FILE" == "$DIR/${FILE//$PATTERN/$REPLACEMENT}"]; then
+                echo "no filename change needed"
+        else
+                git mv "$DIR/$FILE" "$DIR/${FILE//$PATTERN/$REPLACEMENT}"
+        fi
+done < <(exec find \( ! -regex '.*/\..*' \) -type f -printf "%h\t%f\n")
+
+
 find . -name "*.lua" -print | xargs sed -i 's/$1/$2/g'
-for x in `find .`; do git mv $x ${x/$1/$2}; done
