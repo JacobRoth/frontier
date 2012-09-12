c090119c27cab86c2ede8167e60a04446831f7ec
yanom <yanom@usb-hdd-ubuntu.(none)>
2012-09-12 09:38:01 -0500

    still working on renameUnit

--- renameUnit.sh
+++ renameUnit.sh
@@ -1,7 +1,13 @@
 
 #!/bin/bash
 
-find . -name "*.lua" -print | xargs sed -i 's/$1/$2/g' #replaces text in-file
+#find . -name "*.lua" | xargs sed -i 's/$1/$2/g' #replaces text in-file
+
+for infile in `find . -name "*.lua"`
+do
+	sed -i 's/$1/$2/g' infile
+done
+
 for infile in `find . \( ! -regex '.*/\..*' \)`
 do 
 	newname=`echo $infile | sed "s/$1/$2/"`
