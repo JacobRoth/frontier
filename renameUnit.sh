1185010aabd95d9995da18e35a6df6c4028e01b3
Yanom <yanom@rocketmail.com>
2012-09-11 20:10:16 -0500

    ok, if renameUnit.sh doesn't work now, I'm gonna be pissed.

--- renameUnit.sh
+++ renameUnit.sh
@@ -1,8 +1,12 @@
+
 #!/bin/bash
 
-find . -name "*.lua" -print | xargs sed -i 's/$1/$2/g' #replaces text in-file
+#find . -name "*.lua" -print | xargs sed -i 's/$1/$2/g' #replaces text in-file
 for infile in `find . \( ! -regex '.*/\..*' \)`
 do 
 	newname=`echo $infile | sed "s/$1/$2/"`
-	git mv $infile $newname
+	if [ "$infile" != "$newname" ]
+	then 
+		git mv $infile $newname
+	fi
 done
