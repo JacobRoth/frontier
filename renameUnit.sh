efc0c0ace47bf802a9b7adf48c16ef48f1623391
Yanom <yanom@rocketmail.com>
2012-09-11 19:53:58 -0500

    i think i made renameUnit work now

--- renameUnit.sh
+++ renameUnit.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-PATTERN=$1
-REPLACEMENT=$2
-
-IFS=$'\t'
-
-while read DIR FILE; do
-        if ["$DIR/$FILE" == "$DIR/${FILE//$PATTERN/$REPLACEMENT}"]; then
-                echo "no filename change needed"
-        else
-                git mv "$DIR/$FILE" "$DIR/${FILE//$PATTERN/$REPLACEMENT}"
-        fi
-done < <(exec find \( ! -regex '.*/\..*' \) -type f -printf "%h\t%f\n")
-
-
-find . -name "*.lua" -print | xargs sed -i 's/$1/$2/g'
+find . -name "*.lua" -print | xargs sed -i 's/$1/$2/g' #replaces text in-file
+for infile in `find . \( ! -regex '.*/\..*' \)`
+do 
+	newname=`echo $infile | sed "s/$1/$2/"`
+	git mv $infile $newname
+done
